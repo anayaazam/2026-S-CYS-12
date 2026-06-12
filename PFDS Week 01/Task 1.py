5738e562d11017eb1902c5ce25e1ef14bf551b2e
Anaya Azam <266976298+anayaazam@users.noreply.github.com>
2026-06-12 13:21:40 +0500

    Add files via upload

---
+++ PFDS Week 01/Task 1.py
@@ -0,0 +1,4 @@
+# Task 1 : Print Your Information
+print("Name:Anaya Azam")
+print("Department:Computer Engineering")
+print("University: UET Lahore")
---
+++ PFDS Week 01/Task 3.py
@@ -0,0 +1,5 @@
+# Task 3 : Format Output
+# Name:Anaya Azam
+# Age:19
+# City:Lahore
+print("Name:Anaya Azam\nAge: 19\nCity: Lahore")
