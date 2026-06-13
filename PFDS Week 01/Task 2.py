bb3c2e6f36636e61dc23e249053456b81c927fe5
Anaya Azam <266976298+anayaazam@users.noreply.github.com>
2026-06-13 20:01:41 +0500

    Update Task 2.py

--- PFDS Week 01/Task 2.py
+++ PFDS Week 01/Task 2.py
@@ -1,5 +1,21 @@
-# Task 3 : Format Output
-# Name:Anaya Azam
-# Age:19
-# City:Lahore
-print("Name:Anaya Azam\nAge: 19\nCity: Lahore")
+# Task 4 : Take Name and  Age
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print("Name:", name)
+print("Age:", age)
+
+# Task 5 : Identify Data Types
+x=10
+y=3.14
+z=True
+name="UET"
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(name))
+
+print(len(name))
