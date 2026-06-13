4c77b86e906f7a0b1e73616b968d8fb6d8f8c805
Anaya Azam <266976298+anayaazam@users.noreply.github.com>
2026-06-13 14:13:32 +0500

    Delete PFDS Week 04 directory

--- PFDS Week 04/Task 03.py
+++
@@ -1,6 +0,0 @@
-#Task 3: Add a and b
-def add(a,b):
-    return a+b
-a=float(input("Please enter your first number: "))
-b=float(input("Please enter your second number: "))
-print(add(a,b))
--- PFDS Week 04/Task 06.py
+++
@@ -1,6 +0,0 @@
-#Task 6: Student(name,age)
-name= input("Please enter your name: ")
-age= input("Please enter your age: ")
-def student(name,age):
-    return 'The name of the student is',name,'and their age is',age
-print(student(name,age))
