f6c95b61c14f3d542d67ff85125924199e04b4a5
Anaya Azam <266976298+anayaazam@users.noreply.github.com>
2026-06-13 14:15:47 +0500

    Add files via upload

---
+++ PFDS Week 03/Task 1.py
@@ -0,0 +1,24 @@
+# Task 1 : Permutation and Combination
+import math
+
+n= int(input("Enter value of n: "))
+r= int(input("Enter value of r: "))
+
+print("The total number of combinations are",math.comb(n,r))
+print("The total number of permutations are",math.perm(n,r))
+
+#Task 2 : Larger Number and its Multiplication Table
+num1 = int(input("Enter value of 1st number: "))
+num2 = int(input("Enter value of 2nd number: "))
+
+def compare(num1, num2):
+    return max(num1, num2)
+lambda max: max(num1, num2)
+    max = max(num1, num2)
+    print("The greater number is", max)
+
+for i in range(1, 15):
+    product = max * i
+    print(product)
+    i+=1
+
