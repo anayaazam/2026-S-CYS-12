6bae54720b63984a50c9fdb4b454d6ef47a1ce5f
Anaya Azam <266976298+anayaazam@users.noreply.github.com>
2026-06-13 14:01:46 +0500

    Delete PFDS Week 15 directory

--- PFDS Week 15/Task 09.py
+++
@@ -1,9 +0,0 @@
-# Task 9:
-def hello():
-    try:
-        a = input("Enter your name here:")
-        print(f'Hello {a}!')
-        print("Hello World!")
-    except:
-        print("Invalid input")
-print(hello())
--- PFDS Week 15/Task 10.py
+++
@@ -1,9 +0,0 @@
-# Task 10:
-def hello():
-    try:
-        a = input("Enter your name here:")
-        print(f'Hello {a}!')
-        print("Hello World!")
-    except Exception as e:
-        print("Error")
-print(hello())
