0cd0d0edb46148dd3c41fe171431bec7da3080d3
Anaya Azam <266976298+anayaazam@users.noreply.github.com>
2026-06-13 13:59:12 +0500

    Add files via upload

---
+++ PFDS Week 01/Task 1.py
@@ -0,0 +1,8 @@
+# Task 1 : Print Your Information
+print("Name:Anaya Azam")
+print("Department:Computer Engineering")
+print("University: UET Lahore")
+
+# Task 2 : Using sep and end
+print("UET","Lahore","Computer","Engineering",sep="-")
+
