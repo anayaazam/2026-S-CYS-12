1d9682d7309b11c63c51ef5004992313aa3241f4
Anaya Azam <266976298+anayaazam@users.noreply.github.com>
2026-06-13 20:01:18 +0500

    Update Task 1.py

--- PFDS Week 01/Task 1.py
+++ PFDS Week 01/Task 1.py
@@ -6,3 +6,8 @@ print("University: UET Lahore")
 # Task 2 : Using sep and end
 print("UET","Lahore","Computer","Engineering",sep="-")
 
+# Task 3 : Format Output
+# Name:Anaya Azam
+# Age:19
+# City:Lahore
+print("Name:Anaya Azam\nAge: 19\nCity: Lahore")
